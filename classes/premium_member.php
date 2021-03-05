68effa200a748d44380849c42423fda798640763
Ruslan Bessarab <rbessarab@mail.greenriver.edu>
2021-03-05 00:42:48 -0500

    classes added to the composer file

--- classes/premium_member.php
+++ classes/premium_member.php
@@ -17,5 +17,37 @@ class PremiumMember extends Member
         $this->_outDoorInterests = $_outDoorInterests;
     }
 
+    /**
+     * @return array
+     */
+    public function getInDoorInterests()
+    {
+        return $this->_inDoorInterests;
+    }
+
+    /**
+     * @param array $inDoorInterests
+     */
+    public function setInDoorInterests($inDoorInterests)
+    {
+        $this->_inDoorInterests = $inDoorInterests;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOutDoorInterests()
+    {
+        return $this->_outDoorInterests;
+    }
+
+    /**
+     * @param array $outDoorInterests
+     */
+    public function setOutDoorInterests($outDoorInterests)
+    {
+        $this->_outDoorInterests = $outDoorInterests;
+    }
+
 
 }

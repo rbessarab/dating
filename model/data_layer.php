69063b3a91d08cc58411e0a5dea3bb43f2e1f8b0
Ruslan Bessarab <rbessarab@mail.greenriver.edu>
2021-03-17 01:17:58 -0400

    data_layer file added with functions

--- model/data_layer.php
+++ model/data_layer.php
@@ -2,6 +2,48 @@
 
 class DataLayer
 {
+    private $_dbh;
+
+    /**
+     * DataLayer constructor.
+     * @param $dbh
+     */
+    public function __construct($dbh)
+    {
+        $this->_dbh = $dbh;
+    }
+
+    /**
+     * Inserting member's info into database
+     * @param $member
+     */
+    function insertMember($member) {
+
+    }
+
+    /**
+     * Getting all members from database
+     */
+    function getMembers() {
+
+    }
+
+    /**
+     * Getting member's info from database
+     * @param $member_id
+     */
+    function getMember($member_id) {
+
+    }
+
+    /**
+     * Getting member's interests from the database
+     * @param $member_id
+     */
+    function getInterests($member_id) {
+
+    }
+
     /**
      *  @return array
      */
